7f37c1c877f39d42bc60202ee6eeaba203f76657
B1tzzzzz <matew651@gmail.com>
2026-09-12 18:19:49 +0300

    Придумал рещение квадратных уравнений

--- 13_09_26/quad.c
+++ 13_09_26/quad.c
@@ -1,53 +1,81 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <math.h>  
-#include <stdbool.h>
+#include <math.h>
 #define E 1e-12
-
-int sign(int x) {
-    return 1 - 2 * (x < 0);
-}
-
-int signD(double x) {
-    return 1 - 2 * (x < 0);
-}
-void solve() {
-    double a, b, c;
-    printf("Введите коэффициенты квадратного уравнения:");
-    if (scanf("%lf %lf %lf", &a, &b, &c) != 3) {
-        printf("Неверный ввод!");
-        return;
-    }
-
-    int p = fabs(c / a) * signD(c / a), s = fabs(-b / a) * signD(-b / a);
-    //printf("p: %d, s: %d\n", p, s);
-    for (int i = 1; i * i <= abs(p); i++ ){
-        if (abs(p) % i == 0) {
-            int sign1 = 1, sign2 = sign(p);
-            for (int j = 0; j < 2; j++) {
-                if (i * sign1 + (p / i) * sign2 == s) {
-                    if (i == p / i) {
-                        printf("Найден один корень уравнения: %d", i * sign1);
-                    }
-                    else {
-                        printf("Найдено два корня уравнения:\n1-ый корень: %d\n2-ой корень: %d", i * sign1, p / i * sign2);
-                    }
-                    return;
-                }
-                sign1 *= -1;
-                sign2 *= -1;
-            }
+int eq_quadr(double a, double b, double c, double *x1, double *x2) {
+    if (a == 0.0) {
+        if (b == 0.0) {
+            return -2;
+        }
+        else {
+            *x1 = -c / b;
+            *x2 = *x1;
+            return 0; 
         }
     }
 
+    double p = b / a;
+    double q = c / a;
+    double D = p * p - 4.0 * q;
 
-    printf("Решений уравнения нет.");
+    if (D < -E) {
+        *x1 = -p / 2.0;
+        *x2 = sqrt(-D) / 2.0;
+        return -1;
+    } 
+    else if (fabs(D) <= E) {
+        *x1 = -p / 2.0;
+        *x2 = *x1;
+        return 0;
+    } 
+    else {
+        double sqrtD = sqrt(D);
+        double t1, t2;
+        
+        if (p > E) {
+            t1 = (-p - sqrtD) / 2.0;
+        } 
+        else {
+            t1 = (-p + sqrtD) / 2.0;
+        }
+
+        if (fabs(t1) > E) {
+            t2 = q / t1;
+        } 
+        else {
+            t2 = 0.0;
+        }
+        
+        *x1 = t1;
+        *x2 = t2;
+        
+        return 1;
+    }
 }
 
 int main() {
-    int t = 1;
-    while (t--) {
-        solve();
+    double a, b, c;
+    double x1, x2;
+    int res;
+
+    printf("Введите коэффициенты квадратного уравнения: ");
+    if (scanf("%lf %lf %lf", &a, &b, &c) != 3) {
+        printf("Неверный ввод!");
+        return 0;
     }
+    
+    res = eq_quadr(a, b, c, &x1, &x2);
+    if (res == 1) {
+        printf("Найдено 2 корня:\nx1 = %.5lf\nx2 = %.5lf", x1, x2);
+    }
+    else if (res == 0) {
+        printf("Найден 1 корень:\nx1 = %.5lf", x1);
+    }
+    else if (res == -1) {
+        printf("Найден комплексный корень:\n Re:%.5lf\nIm:%.5lf", x1, x2);
+    }
+    else {
+        printf("Корней нет");
+    }
+
     return 0;
 }
